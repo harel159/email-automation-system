375b592d2ddf44ba8bcfbea166c67d3092ad3c05
harel159 <harel.jerbi1@gmail.com>
2025-05-29 15:19:31 +0300

    ✨ Add latest email sent date to authorities table

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,19 +3,21 @@ import db from '../services/db.js';
 export async function getAllAuthorities(req, res) {
   try {
     const result = await db.query(`
-      SELECT 
+    SELECT 
         email,
-        STRING_AGG(name, ', ') AS names
+        STRING_AGG(name, ', ') AS names,
+        MAX("latestInfoDate") AS last_email_sent
       FROM issuer
       WHERE email IS NOT NULL AND name IS NOT NULL
       GROUP BY email
-      ORDER BY names
+      ORDER BY names 
     `);
 
     const authorities = result.rows.map((row, index) => ({
       id: index + 1,
       name: row.names,
-      email: row.email
+      email: row.email,
+      last_email_sent: row.last_email_sent
     }));
 
     res.json(authorities);
