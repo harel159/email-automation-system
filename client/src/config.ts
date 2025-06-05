1f83d080cca253b7bc340658dbc844d520eefd5d
shaiidan <shaiidan17@gmail.com>
2025-06-05 13:11:08 +0300

    update

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const isDev = import.meta.env.DEV;
+const isDev = import.meta.env.VITE_DEV;
 const apiBase = import.meta.env.VITE_EMAIL_AUTOMATION_API_BASE_URL;
 
 export const API_BASE_URL = isDev ? '/api' : `${apiBase}/api`;
