2d80c7e947bf59e9965194b452508b06df78900f
shaiidan <shaiidan17@gmail.com>
2025-06-05 14:30:09 +0300

    update

--- server/server.js
+++ server/server.js
@@ -24,15 +24,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const clientDistPath = path.resolve(__dirname, '../client/dist');
 console.log(clientDistPath.toString());
 
-app.use(express.static(clientDistPath));
-
-app.get('*',(req, res, next) => {
-    if (req.path.startsWith('/api/')) {
-        return next();
-    }
-    console.log("aaaaa");
-    res?.sendFile(path.join(clientDistPath, 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '127.0.0.1', () => console.log(`Server running on port ${PORT}`));
