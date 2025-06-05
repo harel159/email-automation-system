4d5c53eec033a07f1ce0f1c10f4ef602b41c93df
shaiidan <shaiidan17@gmail.com>
2025-06-05 14:00:41 +0300

    update

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
 import dotenv from 'dotenv';
-
 import clientRoutes from './routes/clientRoutes.js';
 import emailRoutes from './routes/emailRoutes.js';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
@@ -18,6 +20,12 @@ app.use('/attachments', express.static('attachments'));
 
 app.use('/api/clients', clientRoutes);
 app.use('/api/email', emailRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+    res?.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '127.0.0.1', () => console.log(`Server running on port ${PORT} only LOCALHOST`));
