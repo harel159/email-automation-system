a267d1d4ef7f7c60e03f3cbdcefd37a2cf30884f
shaiidan <shaiidan17@gmail.com>
2025-06-05 14:28:59 +0300

    update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 
 import clientRoutes from './routes/clientRoutes.js';
 import emailRoutes from './routes/emailRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -18,6 +20,19 @@ app.use('/attachments', express.static('attachments'));
 
 app.use('/api/clients', clientRoutes);
 app.use('/api/email', emailRoutes);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const clientDistPath = path.resolve(__dirname, '../client/dist');
+console.log(clientDistPath.toString());
+
+app.use(express.static(clientDistPath));
+
+app.get('*',(req, res, next) => {
+    if (req.path.startsWith('/api/')) {
+        return next();
+    }
+    console.log("aaaaa");
+    res?.sendFile(path.join(clientDistPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '127.0.0.1', () => console.log(`Server running on port ${PORT}`));
