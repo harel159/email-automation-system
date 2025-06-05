8c4e922889392da4c508432a95bed8ba19faca0a
shaiidan <shaiidan17@gmail.com>
2025-06-05 14:51:17 +0300

    update

--- server/server.js
+++ server/server.js
@@ -20,18 +20,7 @@ app.use('/attachments', express.static('attachments'));
 
 app.use('/api/clients', clientRoutes);
 app.use('/api/email', emailRoutes);
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const clientDistPath = path.resolve(__dirname, '../client/dist');
 
-
-app.use(express.static(clientDistPath));
-
-app.get('*',(req, res) => {
-    if (req.path.startsWith('/api/')) {
-        return res.status(404).send('API route not found');
-    }
-    res?.sendFile(path.join(clientDistPath, 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '127.0.0.1', () => console.log(`Server running on port ${PORT}`));
