187c26808fbbe99c4a0048fed92a37423d2d8546
harel159 <harel.jerbi1@gmail.com>
2025-07-20 20:35:44 +0300

    mail RTL

--- client/src/pages/EmailManager.jsx
+++ client/src/pages/EmailManager.jsx
@@ -208,9 +208,22 @@ export default function EmailManager() {
                   )}
                 </div>
 
-                <Button onClick={handleSend} className="bg-green-600 hover:bg-green-700">
-                  <Send className="w-4 h-4 mr-2" /> Send Email
+                <Button onClick={handleSend} disabled={sending} className="bg-green-600 hover:bg-green-700">
+                  {sending ? (
+                    <span className="flex items-center">
+                      <svg className="animate-spin h-4 w-4 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8z" />
+                      </svg>
+                      Sending...
+                    </span>
+                  ) : (
+                    <>
+                      <Send className="w-4 h-4 mr-2" /> Send Email
+                    </>
+                  )}
                 </Button>
+
 
                 <Button onClick={handleSave} disabled={saving} className="bg-blue-600 hover:bg-blue-700">
                   <Save className="w-4 h-4 mr-2" />
